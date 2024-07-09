f2795eea0f60e1cfdbb93531a1b4ac5b1a2928d8
Sardor Irgashev <irgashevsardor99@gmail.com>
2024-07-09 11:06:35 +0500

    Define existing SQS, EFS infra

--- lib/existing_infra-stack.ts
+++ lib/existing_infra-stack.ts
@@ -1,16 +1,67 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as efs from 'aws-cdk-lib/aws-efs'
+import * as iam from 'aws-cdk-lib/aws-iam'
+import * as ecs from 'aws-cdk-lib/aws-ecs'
 
 export class ExistingInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // const role = new iam.Role(this, 'Role', {
+    //   assumedBy: new iam.ArnPrincipal('arn:aws:iam::408500910341:user/test'),
+    //   roleName: "MyAssumableRole"
+    // })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ExistingInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // inputQueue.grantSendMessages(role)
+
+    const vpc = new ec2.Vpc(this, 'Vpc', { maxAzs: 1 });
+
+    const cluster = new ecs.Cluster(this, 'EcsCluster', { vpc });
+
+    const inputQueue = new sqs.Queue(this, 'NlpSqsQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
+    const nfsIngressAllowSecurityGroup = new ec2.SecurityGroup(this, 'EfsSecurityGroup', { vpc })
+    nfsIngressAllowSecurityGroup.addIngressRule(
+      nfsIngressAllowSecurityGroup,
+      ec2.Port.tcp(2049)
+    )
+
+    const fileSystem = new efs.FileSystem(this, 'NlpV2EfsFileSystem', {
+      vpc: vpc,
+      securityGroup: nfsIngressAllowSecurityGroup
+    });
+
+    fileSystem.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ["elasticfilesystem:ClientMount"],
+      effect: iam.Effect.ALLOW,
+      resources: ["*"],
+      principals: [new iam.ArnPrincipal("arn:aws:iam::408500910341:user/nlp_user2")]
+    }
+    ))
+
+    new cdk.CfnOutput(this, 'SQS Name', {
+      value: inputQueue.queueName,
+    })
+
+    new cdk.CfnOutput(this, 'SQS ARN', {
+      value: inputQueue.queueArn
+    })
+
+    new cdk.CfnOutput(this, 'FileSystemId', {
+      value: fileSystem.fileSystemId
+    })
+
+    new cdk.CfnOutput(this, 'Cluster Name', {
+      value: cluster.clusterName
+    })
+
+    new cdk.CfnOutput(this, 'VPC Id', {
+      value: vpc.vpcId
+    })
   }
 }
